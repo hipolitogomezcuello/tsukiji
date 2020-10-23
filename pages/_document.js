32b72d8b4819732e4bb1c46f40ce63ae02c0e179
hipolitogomezcuello@gmail.com <hipolitogomezcuello@gmail.com>
2020-10-23 17:13:33 -0300

    initial commit

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Document, {
+  Html, Main, NextScript, Head
+} from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
